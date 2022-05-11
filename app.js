8426b00bcde70120766b72f82962e8b324fb9b34
GUSSCH <steamjg04@gmail.com>
2022-05-11 15:45:46 +0200

    white footer text color

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-// show text on click --> https://html-shark.com/HTML/ShowHideSections.htm
-
 let shit = false;
-
 function showmyshit() {
   var height_input = document.getElementById("body-info");
   if (shit === false) {
@@ -9,6 +6,6 @@ function showmyshit() {
     height_input.style.display = "block";
   } else {
     height_input.style.display = "none";
-    shit = false
+    shit = false;
   }
 }
